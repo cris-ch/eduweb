def6797e67702cf8f9f845ad928a99186c1849ee
Cristian <cristian.christensen@gmail.com>
2024-05-14 13:57:24 +0200

    added scrolling bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -143,3 +143,15 @@ function removeHighlights() {
 
 // Scrolling bar
 
+// Scrolling Bar
+const body = document.body;
+const progressBar = document.querySelector('.progress__bar');
+
+const updateProgress = () => {
+  let scrollPos =
+    (window.scrollY / (body.scrollHeight - window.innerHeight)) * 100;
+  progressBar.style.width = `${scrollPos}%`;
+  requestAnimationFrame(updateProgress);
+};
+
+updateProgress();
